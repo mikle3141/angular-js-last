6ec064d341cf8b2076a72a768cf0d207bb5a7e8d
mikle3141 <mikle.chivozertsev@gmail.com>
2026-04-28 12:54:15 +0300

    Адаптировал сборку под сборку на внешнем контуре. Исправил опечатку в plugin.xml
    Но сборка плагина теперь дает ошибку:
    * What went wrong:
    Plugin [id: 'org.jetbrains.intellij.platform', version: '2.6.0'] was not found in any of the following sources:
    
    - Gradle Core Plugins (plugin is not in 'org.gradle' namespace)
    - Included Builds (No included builds contain this plugin)
    - Plugin Repositories (could not resolve plugin artifact 'org.jetbrains.intellij.platform:org.jetbrains.intellij.platform.gradle.plugin:2.6.0')
      Searched in the following repositories:
        MavenRepo

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,96 +33,9 @@ version = properties("pluginVersion")
 version = if (isRelease) version else "$version-SNAPSHOT"
 
 repositories {
-    maven {
-        name = "sberosc-intellij-repository-releases"
-        url = uri("https://sberosc.sigma.sbrf.ru/repo/maven/jetbrains_intellij_repository/releases")
-        credentials {
-            username = "token"
-            password = oscToken
-        }
-    }
-    maven {
-        name = "public"
-        url = uri("https://nexus-ci.delta.sbrf.ru/repository/public/")
-        credentials {
-            username = mavenUser
-            password = mavenPassword
-        }
-    }
-    maven {
-        name = "central"
-        url = uri("https://nexus-ci.delta.sbrf.ru/repository/central/")
-        credentials {
-            username = mavenUser
-            password = mavenPassword
-        }
-        content {
-            includeGroup("com.google.code.gson")
-        }
-    }
-    maven {
-        name = "maven-lib-int"
-        url = uri("https://nexus-ci.delta.sbrf.ru/repository/maven-lib-int")
-        credentials {
-            username = mavenUser
-            password = mavenPassword
-        }
-        metadataSources {
-            mavenPom()
-            gradleMetadata()
-        }
-    }
-    maven {
-        name = "maven-sberosc-cache"
-        url = uri("https://nexus-ci.delta.sbrf.ru/repository/maven-sberosc-cache")
-        credentials {
-            username = mavenUser
-            password = mavenPassword
-        }
-        metadataSources {
-            mavenPom()
-            gradleMetadata()
-        }
-    }
-    maven {
-        name = "maven-sberosc-cache-intellij-dependencies"
-        url = uri("https://nexus-ci.delta.sbrf.ru/repository/maven-sberosc-cache/intellij-dependencies")
-        credentials {
-            username = mavenUser
-            password = mavenPassword
-        }
-        metadataSources {
-            mavenPom()
-            gradleMetadata()
-        }
-    }
-    maven {
-        name = "sberosc-intellij-dependencies"
-        url = uri("https://sberosc.sigma.sbrf.ru/repo/maven/jetbrains_redirect/intellij-dependencies/")
-        credentials {
-            username = "token"
-            password = oscToken
-        }
-    }
-    maven {
-        name = "sberosc-central"
-        url = uri("https://sberosc.sigma.sbrf.ru/repo/maven/central/")
-        credentials {
-            username = "token"
-            password = oscToken
-        }
-    }
-    maven {
-        name = "sberosc-intellij-repository"
-        url = uri("https://sberosc.sigma.sbrf.ru/repo/maven/jetbrains_intellij_repository/")
-        credentials {
-            username = "token"
-            password = oscToken
-        }
-    }
-
+    mavenCentral()
     intellijPlatform {
-        localPlatformArtifacts()
+        defaultRepositories()
     }
 }
 
@@ -157,39 +70,39 @@ tasks.named<org.gradle.api.plugins.antlr.AntlrTask>("generateGrammarSource") {
     arguments = arguments + listOf("-package", "org.antlr.jetbrains.sample.parser", "-Xexact-output-dir")
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            artifactId = properties("pluginId")
-            groupId = project.group as String
-            version = project.version as String
-            artifact("build/distributions/${project.name}-${project.version}.zip") {
-                extension = "zip"
-            }
-        }
-    }
-    repositories {
-        maven {
-            val releaseUrl = "https://nexus-ci.delta.sbrf.ru/repository/maven-lib-release"
-            val snapshotUrl = "https://nexus-ci.delta.sbrf.ru/repository/maven-lib-dev"
-            url = uri(if (isRelease) releaseUrl else snapshotUrl)
-            credentials {
-                username = System.getProperty("gradle.wrapperUser")
-                password = System.getProperty("gradle.wrapperPassword")
-            }
-        }
-    }
-}
-
-tasks.named("publishAllPublicationsToMavenRepository") {
-    dependsOn("buildPlugin")
-}
-tasks.named("publishMavenPublicationToMavenLocal") {
-    dependsOn("buildPlugin")
-}
-tasks.named("publishMavenPublicationToMavenRepository") {
-    dependsOn("buildPlugin")
-}
-tasks.named("publishToMavenLocal") {
-    dependsOn("buildPlugin")
-}
+//publishing {
+//    publications {
+//        create<MavenPublication>("maven") {
+//            artifactId = properties("pluginId")
+//            groupId = project.group as String
+//            version = project.version as String
+//            artifact("build/distributions/${project.name}-${project.version}.zip") {
+//                extension = "zip"
+//            }
+//        }
+//    }
+//    repositories {
+//        maven {
+//            val releaseUrl = "https://nexus-ci.delta.sbrf.ru/repository/maven-lib-release"
+//            val snapshotUrl = "https://nexus-ci.delta.sbrf.ru/repository/maven-lib-dev"
+//            url = uri(if (isRelease) releaseUrl else snapshotUrl)
+//            credentials {
+//                username = System.getProperty("gradle.wrapperUser")
+//                password = System.getProperty("gradle.wrapperPassword")
+//            }
+//        }
+//    }
+//}
+//
+//tasks.named("publishAllPublicationsToMavenRepository") {
+//    dependsOn("buildPlugin")
+//}
+//tasks.named("publishMavenPublicationToMavenLocal") {
+//    dependsOn("buildPlugin")
+//}
+//tasks.named("publishMavenPublicationToMavenRepository") {
+//    dependsOn("buildPlugin")
+//}
+//tasks.named("publishToMavenLocal") {
+//    dependsOn("buildPlugin")
+//}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,40 +1,6 @@
 pluginManagement {
     repositories {
-        val oscToken: String = System.getProperty("gradle.wrapperOscToken")
-        val mavenUser: String = System.getProperty("gradle.wrapperUser")
-        val mavenPassword: String = System.getProperty("gradle.wrapperPassword")
-
-        maven {
-            name = "public"
-            url = uri("https://nexus-ci.delta.sbrf.ru/repository/public")
-            credentials {
-                username = mavenUser
-                password = mavenPassword
-            }
-            content {
-                includeGroup("com.google.code.gson")
-            }
-        }
-        maven {
-            name = "gradle-plugins"
-            url = uri("https://sberosc.sigma.sbrf.ru/repo/maven/gradle_plugins")
-            credentials {
-                username = "token"
-                password = oscToken
-            }
-        }
-        maven {
-            name = "epoch"
-            url = uri("https://nexus-ci.delta.sbrf.ru/repository/maven-lib-int/ru/sbrf/epoch")
-            credentials {
-                username = mavenUser
-                password = mavenPassword
-            }
-            metadataSources {
-                mavenPom()
-                gradleMetadata()
-            }
-        }
+        mavenCentral()
     }
 }
 
